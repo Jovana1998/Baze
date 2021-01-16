a764397f0f1ceb043a31140d956bd32454efe860
Vidosava98 <74729669+Vidosava98@users.noreply.github.com>
2021-01-16 23:23:08 +0100

    kreiran projekat, povezano sa bazom

---
+++ BazeNeo4J/Teretane/Teretane/DomainModel/Trener.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teretane.DomainModel
+{
+    public class Trener
+    {
+        public String id { get; set; }
+        public String ime { get; set; }
+        public String prezime { get; set; }
+        public String ocena { get; set; }
+        
+    }
+}
