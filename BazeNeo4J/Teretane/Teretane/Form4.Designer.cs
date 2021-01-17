26502001fe4acc17d9854ba03c6831134619f671
Katarina4444 <76787593+Katarina4444@users.noreply.github.com>
2021-01-17 17:29:14 +0100

    Gotova forma

--- BazeNeo4J/Teretane/Teretane/Form4.Designer.cs
+++ BazeNeo4J/Teretane/Teretane/Form4.Designer.cs
@@ -29,19 +29,155 @@ namespace Teretane
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Uclani_se_k = new System.Windows.Forms.Button();
+            this.Odustani = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(112, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(304, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(112, 251);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(304, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(112, 199);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(304, 26);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(112, 144);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(304, 26);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(112, 91);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(304, 26);
+            this.textBox5.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ime:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Prezime:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Kilogrami:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Visina:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(68, 257);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Pol:";
+            // 
+            // Uclani_se_k
+            // 
+            this.Uclani_se_k.Location = new System.Drawing.Point(112, 347);
+            this.Uclani_se_k.Name = "Uclani_se_k";
+            this.Uclani_se_k.Size = new System.Drawing.Size(137, 43);
+            this.Uclani_se_k.TabIndex = 10;
+            this.Uclani_se_k.Text = "Uclani se";
+            this.Uclani_se_k.UseVisualStyleBackColor = true;
+            // 
+            // Odustani
+            // 
+            this.Odustani.Location = new System.Drawing.Point(274, 347);
+            this.Odustani.Name = "Odustani";
+            this.Odustani.Size = new System.Drawing.Size(142, 43);
+            this.Odustani.TabIndex = 11;
+            this.Odustani.Text = "Odustani";
+            this.Odustani.UseVisualStyleBackColor = true;
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(455, 406);
+            this.Controls.Add(this.Odustani);
+            this.Controls.Add(this.Uclani_se_k);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form4";
             this.Text = "Uclani se";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Uclani_se_k;
+        private System.Windows.Forms.Button Odustani;
     }
 }
--- BazeNeo4J/Teretane/Teretane/Form5.Designer.cs
+++ BazeNeo4J/Teretane/Teretane/Form5.Designer.cs
@@ -29,19 +29,182 @@ namespace Teretane
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.Dodaj_trenera = new System.Windows.Forms.Button();
+            this.Obrisi_trenera = new System.Windows.Forms.Button();
+            this.Izmeni_trenera = new System.Windows.Forms.Button();
+            this.Vrati_se_na_pocetak = new System.Windows.Forms.Button();
+            this.Obrisi_teretanu = new System.Windows.Forms.Button();
+            this.Dodaj_teretanu = new System.Windows.Forms.Button();
+            this.Izmeni_teretanu = new System.Windows.Forms.Button();
+            this.Obrisi_korisnika = new System.Windows.Forms.Button();
+            this.Dodaj_korisnika = new System.Windows.Forms.Button();
+            this.Izmeni_korisnika = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Izmeni_trenera);
+            this.groupBox1.Controls.Add(this.Obrisi_trenera);
+            this.groupBox1.Controls.Add(this.Dodaj_trenera);
+            this.groupBox1.Location = new System.Drawing.Point(304, 48);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 213);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Trener";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Izmeni_teretanu);
+            this.groupBox2.Controls.Add(this.Dodaj_teretanu);
+            this.groupBox2.Controls.Add(this.Obrisi_teretanu);
+            this.groupBox2.Location = new System.Drawing.Point(44, 48);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 213);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Teretana";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.Izmeni_korisnika);
+            this.groupBox3.Controls.Add(this.Dodaj_korisnika);
+            this.groupBox3.Controls.Add(this.Obrisi_korisnika);
+            this.groupBox3.Location = new System.Drawing.Point(569, 48);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 213);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Korisnik";
+            // 
+            // Dodaj_trenera
+            // 
+            this.Dodaj_trenera.Location = new System.Drawing.Point(6, 39);
+            this.Dodaj_trenera.Name = "Dodaj_trenera";
+            this.Dodaj_trenera.Size = new System.Drawing.Size(188, 36);
+            this.Dodaj_trenera.TabIndex = 0;
+            this.Dodaj_trenera.Text = "Dodaj trenera";
+            this.Dodaj_trenera.UseVisualStyleBackColor = true;
+            // 
+            // Obrisi_trenera
+            // 
+            this.Obrisi_trenera.Location = new System.Drawing.Point(6, 88);
+            this.Obrisi_trenera.Name = "Obrisi_trenera";
+            this.Obrisi_trenera.Size = new System.Drawing.Size(188, 36);
+            this.Obrisi_trenera.TabIndex = 1;
+            this.Obrisi_trenera.Text = "Obrisi trenera";
+            this.Obrisi_trenera.UseVisualStyleBackColor = true;
+            // 
+            // Izmeni_trenera
+            // 
+            this.Izmeni_trenera.Location = new System.Drawing.Point(6, 139);
+            this.Izmeni_trenera.Name = "Izmeni_trenera";
+            this.Izmeni_trenera.Size = new System.Drawing.Size(188, 36);
+            this.Izmeni_trenera.TabIndex = 2;
+            this.Izmeni_trenera.Text = "Izmeni trenera";
+            this.Izmeni_trenera.UseVisualStyleBackColor = true;
+            // 
+            // Vrati_se_na_pocetak
+            // 
+            this.Vrati_se_na_pocetak.Location = new System.Drawing.Point(569, 292);
+            this.Vrati_se_na_pocetak.Name = "Vrati_se_na_pocetak";
+            this.Vrati_se_na_pocetak.Size = new System.Drawing.Size(200, 47);
+            this.Vrati_se_na_pocetak.TabIndex = 3;
+            this.Vrati_se_na_pocetak.Text = "Vrati se na pocetak";
+            this.Vrati_se_na_pocetak.UseVisualStyleBackColor = true;
+            // 
+            // Obrisi_teretanu
+            // 
+            this.Obrisi_teretanu.Location = new System.Drawing.Point(6, 88);
+            this.Obrisi_teretanu.Name = "Obrisi_teretanu";
+            this.Obrisi_teretanu.Size = new System.Drawing.Size(188, 36);
+            this.Obrisi_teretanu.TabIndex = 1;
+            this.Obrisi_teretanu.Text = "Obrisi teretanu";
+            this.Obrisi_teretanu.UseVisualStyleBackColor = true;
+            // 
+            // Dodaj_teretanu
+            // 
+            this.Dodaj_teretanu.Location = new System.Drawing.Point(6, 39);
+            this.Dodaj_teretanu.Name = "Dodaj_teretanu";
+            this.Dodaj_teretanu.Size = new System.Drawing.Size(188, 36);
+            this.Dodaj_teretanu.TabIndex = 2;
+            this.Dodaj_teretanu.Text = "Dodaj teretanu";
+            this.Dodaj_teretanu.UseVisualStyleBackColor = true;
+            // 
+            // Izmeni_teretanu
+            // 
+            this.Izmeni_teretanu.Location = new System.Drawing.Point(6, 139);
+            this.Izmeni_teretanu.Name = "Izmeni_teretanu";
+            this.Izmeni_teretanu.Size = new System.Drawing.Size(188, 36);
+            this.Izmeni_teretanu.TabIndex = 3;
+            this.Izmeni_teretanu.Text = "Izmeni teretanu";
+            this.Izmeni_teretanu.UseVisualStyleBackColor = true;
+            // 
+            // Obrisi_korisnika
+            // 
+            this.Obrisi_korisnika.Location = new System.Drawing.Point(6, 88);
+            this.Obrisi_korisnika.Name = "Obrisi_korisnika";
+            this.Obrisi_korisnika.Size = new System.Drawing.Size(188, 36);
+            this.Obrisi_korisnika.TabIndex = 1;
+            this.Obrisi_korisnika.Text = "Obrisi korisnika";
+            this.Obrisi_korisnika.UseVisualStyleBackColor = true;
+            // 
+            // Dodaj_korisnika
+            // 
+            this.Dodaj_korisnika.Location = new System.Drawing.Point(6, 39);
+            this.Dodaj_korisnika.Name = "Dodaj_korisnika";
+            this.Dodaj_korisnika.Size = new System.Drawing.Size(188, 36);
+            this.Dodaj_korisnika.TabIndex = 2;
+            this.Dodaj_korisnika.Text = "Dodaj korisnika";
+            this.Dodaj_korisnika.UseVisualStyleBackColor = true;
+            // 
+            // Izmeni_korisnika
+            // 
+            this.Izmeni_korisnika.Location = new System.Drawing.Point(6, 139);
+            this.Izmeni_korisnika.Name = "Izmeni_korisnika";
+            this.Izmeni_korisnika.Size = new System.Drawing.Size(188, 36);
+            this.Izmeni_korisnika.TabIndex = 3;
+            this.Izmeni_korisnika.Text = "Izmeni korisnika";
+            this.Izmeni_korisnika.UseVisualStyleBackColor = true;
             // 
             // Form5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(851, 353);
+            this.Controls.Add(this.Vrati_se_na_pocetak);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form5";
             this.Text = "Admin";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Izmeni_trenera;
+        private System.Windows.Forms.Button Obrisi_trenera;
+        private System.Windows.Forms.Button Dodaj_trenera;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button Izmeni_teretanu;
+        private System.Windows.Forms.Button Dodaj_teretanu;
+        private System.Windows.Forms.Button Obrisi_teretanu;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button Izmeni_korisnika;
+        private System.Windows.Forms.Button Dodaj_korisnika;
+        private System.Windows.Forms.Button Obrisi_korisnika;
+        private System.Windows.Forms.Button Vrati_se_na_pocetak;
     }
 }
