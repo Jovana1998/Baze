e59e64a68c5c62360c7d847fce4be61d24a20e10
JovanaJovic98 <jovic.jovana@elfak.rs>
2021-01-18 20:23:37 +0100

    Korisnik moze da se uclani

--- BazeNeo4J/Baze_Teretane/Baze_Teretane/Pages/UclaniSe.cshtml.cs
+++ BazeNeo4J/Baze_Teretane/Baze_Teretane/Pages/UclaniSe.cshtml.cs
@@ -14,8 +14,14 @@ namespace Baze_Teretane.Pages
     {
         private readonly ILogger<UclaniSeModel> _logger;
         BoltGraphClient client;
-
+        [BindProperty]
         public Korisnik NoviKorisnik { get; set; }
+        public List<Usluga> SveUsluge { get; set; }
+        public List<Trener> SviTreneri { get; set; }
+        [BindProperty]
+        public int idTrenera { get; set; }
+        [BindProperty]
+        public int idUsluge { get; set; }
         public UclaniSeModel(ILogger<UclaniSeModel> logger)
         {
             client = Manager.GetClient();
@@ -40,11 +46,42 @@ namespace Baze_Teretane.Pages
             int pom = Int32.Parse(maxId);
             pom++;
             NoviKorisnik.id = pom.ToString();
-            //Pravimo vezu 
+            //Pravimo vezu o korisnika ka teretani 
             Dictionary<string, object> queryDict1 = new Dictionary<string, object>();
             var query1 = new Neo4jClient.Cypher.CypherQuery("MATCH(a: Korisnik), (b: Teretana) WHERE a.id = '"+NoviKorisnik.id+"' AND b.id = '"+id+"' CREATE(a) -[r: CLAN]->(b) RETURN a", queryDict1, CypherResultMode.Set);
             Korisnik k = ((IRawGraphClient)client).ExecuteGetCypherResults<Korisnik>(query1).FirstOrDefault();
+
+            //trazimo sve usluge ove teretane zbog select
+            Dictionary<string, object> queryDict2 = new Dictionary<string, object>();
+            var query2 = new Neo4jClient.Cypher.CypherQuery("MATCH p=(n)-[r:NUDI_USLUGU]->(m) where n.id= '" + ter.id + "' RETURN m", queryDict2, CypherResultMode.Set);
+            SveUsluge = ((IRawGraphClient)client).ExecuteGetCypherResults<Usluga>(query2).ToList();
+
+            //trazimo sve trenere zbog select
+            Dictionary<string, object> queryDict3 = new Dictionary<string, object>();
+            var query3 = new Neo4jClient.Cypher.CypherQuery("MATCH p=(n)-[r:RADI_U]->(m) where m.id= '" + ter.id + "' RETURN n", queryDict3, CypherResultMode.Set);
+            SviTreneri = ((IRawGraphClient)client).ExecuteGetCypherResults<Trener>(query3).ToList();
             return Page();
         }
+        public async Task<IActionResult> OnPostUclaniAsync()
+        { //prvo unosim podatke o korisniku u bazu 
+            Dictionary<string, object> queryDict = new Dictionary<string, object>();
+            var query = new Neo4jClient.Cypher.CypherQuery("MATCH(n: Korisnik { id: '" + NoviKorisnik.id + "' })" +
+                " SET n.ime = '" + NoviKorisnik.ime + "', n.prezime = '" + NoviKorisnik.prezime + "', n.kilogram='" + NoviKorisnik.kilogram + "', n.visina= '" + NoviKorisnik.visina + "', n.bolesti= '" + NoviKorisnik.bolesti + "', n.pol= '" + NoviKorisnik.pol + "'  " +
+                "RETURN n", queryDict, CypherResultMode.Set);
+            Korisnik korisnik = ((IRawGraphClient)client).ExecuteGetCypherResults<Korisnik>(query).FirstOrDefault();
+
+            //izabrani trener veza sa korisnikom
+            Dictionary<string, object> queryDict1 = new Dictionary<string, object>();
+            var query1 = new Neo4jClient.Cypher.CypherQuery("MATCH(k: Korisnik { id: '"+NoviKorisnik.id+"'}), (t: Trener { id: '"+idTrenera+"'}) WITH k, t CREATE(k) -[:VEZBA_SA]->(t) return k", queryDict1, CypherResultMode.Set);
+            Korisnik k = ((IRawGraphClient)client).ExecuteGetCypherResults<Korisnik>(query1).FirstOrDefault();
+            //veza usluga korisnik
+            Dictionary<string, object> queryDict2 = new Dictionary<string, object>();
+            var query2 = new Neo4jClient.Cypher.CypherQuery("MATCH (k:Korisnik {id: '"+NoviKorisnik.id+"'}), (u:Usluga {id: '"+idUsluge+"'}) WITH k, u CREATE (k)-[:KORISTI]->(u) return k", queryDict2, CypherResultMode.Set);
+            Korisnik a = ((IRawGraphClient)client).ExecuteGetCypherResults<Korisnik>(query2).FirstOrDefault();
+
+
+            return Page();
+        }
+
     }
 }
