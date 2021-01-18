af4951b1a8b291407e5a58c861bccc2b2e31871b
Katarina4444 <76787593+Katarina4444@users.noreply.github.com>
2021-01-18 14:46:16 +0100

    t

---
+++ BazeNeo4J/Baze_Teretane/Baze_Teretane/Node/Plan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Baze_Teretane
+{
+    public class Plan
+    {
+        public String id { get; set; }
+        public String opisplana { get; set; }
+    }
+}
