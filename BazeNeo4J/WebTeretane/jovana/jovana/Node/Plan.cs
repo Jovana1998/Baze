d299ade2288bf5f956a8da7fdc1e3a378636ecfc
Vidosava98 <74729669+Vidosava98@users.noreply.github.com>
2021-01-18 14:50:11 +0100

    b

---
+++ BazeNeo4J/WebTeretane/jovana/jovana/Node/Plan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jovana
+{
+    public class Plan
+    {
+        public String id { get; set; }
+        public String opisplana { get; set; }
+    }
+}
---
+++ BazeNeo4J/WebTeretane/jovana/jovana/Node/Usluga.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jovana
+{
+    public class Usluga
+    {
+        public String id { get; set; }
+        public String nazivusluge { get; set; }
+    }
+}
